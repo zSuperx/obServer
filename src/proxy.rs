9f4b21b0b1b7b890709407317614a554667992d2
zsuper <piyushkumbhare10@gmail.com>
2026-03-07 13:02:05 -0800

    moved "protobuf" into its own library crate
    its so much cleaner now!

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,14 +1,25 @@
+// Structure of a Minecraft packet:
+// -------------------------
+// | Field     | Type      |
+// -------------------------
+// | Length    | VarInt    |
+// | Packet ID | VarInt    |
+// | Data      | Byte Arr  |
+// -------------------------
+
 use std::{
-    io::{BufReader, Read, Write},
+    io::{BufReader, BufWriter, Read, Write},
     net::{SocketAddr, TcpListener, TcpStream},
+    sync::{Arc, Mutex, OnceLock, RwLock, mpsc},
 };
 
-use anyhow::bail;
+use anyhow::{anyhow, bail};
 use socket2::Socket;
 
-use crate::datacoding::*;
 use crate::rwbuf::{MyBufReader, MyBufWriter};
+use protobuf::{VarRead, VarWrite};
 
+const WIDTH: usize = 35;
 const EXAMPLE_RESPONSE: &'static str = r#"{
     "version": {
         "name": "obServer",
@@ -22,137 +33,175 @@ const EXAMPLE_RESPONSE: &'static str = r#"{
     "description": {
         "text": "Server is down. Login to start it up!"
     },
-    "favicon": "data:image/png;base64,<data>",
+    "favicon": "data:image/png;base64,<img-data>",
     "enforcesSecureChat": false
 }"#;
 
-pub struct MCProxy;
+static BLITTY_RESPONSE: OnceLock<String> = OnceLock::new();
 
-impl MCProxy {
-    pub fn run(port: u16) -> anyhow::Result<()> {
-        // create a TCP listener
-        let socket = Socket::new(socket2::Domain::IPV4, socket2::Type::STREAM, None)?;
+#[derive(Debug)]
+enum Event {
+    ClientJoined(TcpStream),
+    Shutdown,
+}
 
-        let address: SocketAddr = format!("127.0.0.1:{port}").parse().unwrap();
-        let address = address.into();
-        socket.set_reuse_address(true);
-        socket.bind(&address)?;
-        socket.listen(128)?;
-        let listener: TcpListener = socket.into();
+/// Runs the Minecraft proxy server, spawning threads for each client that interacts with it
+pub fn run_server(port: u16) -> anyhow::Result<()> {
+    // create the blitty
+    let blitty_str = EXAMPLE_RESPONSE.replace(
+        "<img-data>",
+        std::fs::read_to_string("./assets/blitty.b64")?.as_str(),
+    );
+    BLITTY_RESPONSE.set(blitty_str);
 
+    // create a TCP listener
+    let socket = Socket::new(socket2::Domain::IPV4, socket2::Type::STREAM, None)?;
+
+    let address: SocketAddr = format!("127.0.0.1:{port}").parse().unwrap();
+    let address = address.into();
+    socket.set_reuse_address(true);
+    socket.bind(&address)?;
+    socket.listen(128)?;
+    let listener: TcpListener = socket.into();
+
+    let (tx, rx) = mpsc::channel::<Event>();
+
+    let listener_tx = tx.clone();
+    // This thread will forever listen on the TcpListener and signal the main thread upon
+    // connections
+    std::thread::spawn(move || {
         for client in listener.incoming() {
-            println!("Accepted client!");
-            let stream = client?;
-            std::thread::spawn(move || {
-                if let Err(e) = ClientHandler::new(stream).run() {
-                    println!("{e}");
-                }
-            });
+            match client {
+                Ok(stream) => listener_tx.send(Event::ClientJoined(stream)).unwrap(),
+                Err(e) => println!("[ERROR] While attempting to connect to client: {e}"),
+            }
         }
+    });
 
-        Ok(())
+    // Now just loop on incoming events so we know whether to create a new proxy session thread or
+    // if we need to exit the loop and return control back to the main application
+    loop {
+        match rx.recv()? {
+            Event::ClientJoined(tcp_stream) => {
+                let thread_tx = tx.clone();
+                std::thread::spawn(move || match ProxySession::new(&tcp_stream).run() {
+                    Ok(_) => thread_tx.send(Event::Shutdown).unwrap(),
+                    Err(e) => println!("[ERROR] While talking to client: {e}"),
+                });
+            }
+            Event::Shutdown => return Ok(()),
+        }
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-enum ServerState {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ProxyState {
     Status,
     Login,
     Transfer,
     Handshaking,
 }
 
-pub struct ClientHandler {
-    state: ServerState,
+pub struct ProxySession<'a> {
+    state: ProxyState,
     exit: bool,
-    stream: MyBufReader<MyBufWriter<TcpStream>>,
+    reader: BufReader<&'a TcpStream>,
+    writer: BufWriter<&'a TcpStream>,
+    packet_buf: Vec<u8>,
 }
 
-impl ClientHandler {
-    pub fn new(stream: TcpStream) -> Self {
+impl<'a> ProxySession<'a> {
+    pub fn new<'tcp: 'a>(stream: &'tcp TcpStream) -> Self {
         Self {
-            state: ServerState::Handshaking,
+            state: ProxyState::Handshaking,
+            reader: BufReader::new(&stream),
+            writer: BufWriter::new(&stream),
+            packet_buf: vec![],
             exit: false,
-            stream: MyBufReader::new(MyBufWriter::new(stream)),
         }
     }
 
     pub fn run(&mut self) -> anyhow::Result<()> {
         while !self.exit {
             // Get packet length
-            // We use the optional function here because it's the only place in the protocol that
-            // expects an EOF. Everywhere else should treat a closed pipe as an error.
-            let Some(packet_length) = read_var_int_optional(&mut self.stream, 3)? else {
-                bail!("Client left!");
-            };
+            let packet_length: i32 = self
+                .reader
+                .read_var_int()
+                .map_err(|_| anyhow!("Client left!"))?;
 
-            println!("\n=== {:?} ===", self.state);
-            println!("Packet length: {packet_length}");
+            println!("{:=^width$}", format!(" {:?} ", self.state), width = WIDTH);
+            printkv("Packet length", packet_length);
 
-            // Allocate & write to packet buffer
-            let mut data_buf = vec![0u8; packet_length as usize];
-            self.stream.read_exact(&mut data_buf)?;
-            let data_reader = BufReader::new(data_buf.as_slice());
+            // Allocate & read full packet into buffer
+            self.packet_buf.resize(packet_length as usize, 0x00);
+            self.reader.read_exact(&mut self.packet_buf)?;
+
+            let old_state = self.state;
 
             // Hand off packet data to specified handler
             match self.state {
-                ServerState::Status => self.process_status(data_reader),
-                ServerState::Login => self.process_login(data_reader),
-                ServerState::Transfer => self.process_status(data_reader),
-                ServerState::Handshaking => self.process_handshaking(data_reader),
+                ProxyState::Status => self.process_status(),
+                ProxyState::Login => self.process_login(),
+                ProxyState::Transfer => self.process_status(),
+                ProxyState::Handshaking => self.process_handshaking(),
             }?;
+
+            println!("{:=<width$}\n", "", width = WIDTH);
+
+            if old_state != self.state {
+                println!("[STATE CHANGE] {:?} -> {:?}\n", old_state, self.state);
+            }
         }
+
+        println!("[PROXY] Closed TcpStream\n");
         Ok(())
     }
 
-    fn process_handshaking(&mut self, mut data: BufReader<&[u8]>) -> anyhow::Result<()> {
-        let packet_id = read_var_int(&mut data, 5)?;
-        println!("Packet ID: {packet_id}");
-
+    fn process_handshaking(&mut self) -> anyhow::Result<()> {
+        let mut data = self.packet_buf.as_slice();
+        let packet_id = data.read_var_int()?;
+        printkv("Packet ID", packet_id);
         match packet_id {
             0x00 => {
-                // Read
-                let protocol_version = read_var_int(&mut data, 5)?;
-                println!("Protocol Version: {protocol_version}");
+                let protocol_version: i32 = data.read_var_int()?;
+                printkv("Protocol Version", protocol_version);
 
-                let s = read_var_string(&mut data)?;
-                println!("Server Address: {s}");
+                let s = data.read_var_string()?;
+                printkv("Server Address", s);
 
                 let mut bytes = [0u8; 2];
                 data.read_exact(&mut bytes)?;
                 let port = u16::from_be_bytes(bytes);
-                println!("Server Port: {port}");
+                printkv("Server Port", port);
 
-                let intent = read_var_int(&mut data, 5)?;
-                println!("Intent: {intent}");
+                let intent = data.read_var_int()?;
+                printkv("Intent", intent);
 
                 let new_state = match intent {
-                    1 => ServerState::Status,
-                    2 => ServerState::Login,
-                    3 => ServerState::Transfer,
+                    1 => ProxyState::Status,
+                    2 => ProxyState::Login,
+                    3 => ProxyState::Transfer,
                     _ => bail!("Unknown status enum: {intent}"),
                 };
 
                 self.state = new_state;
-
-                println!("Server state: {:?} -> {:?}", self.state, new_state);
             }
             x => bail!("Unknown packet ID: {x}"),
         }
         Ok(())
     }
 
-    fn process_status(&mut self, mut data: BufReader<&[u8]>) -> anyhow::Result<()> {
-        let packet_id = read_var_int(&mut data, 5)?;
-        println!("Packet ID: {packet_id}");
-
+    fn process_status(&mut self) -> anyhow::Result<()> {
+        let mut data = self.packet_buf.as_slice();
+        let packet_id = data.read_var_int()?;
+        printkv("Packet ID", packet_id);
         match packet_id {
             0x00 => {
                 // Respond with status
                 let mut response = Vec::new();
                 response.write_all(&[0x00])?;
-                write_var_string(&mut response, EXAMPLE_RESPONSE)?;
-                write_response(&mut self.stream, &response)?;
+                response.write_var_string(BLITTY_RESPONSE.get().unwrap())?;
+                self.writer.write_response(&response)?;
                 println!("Responded with status");
             }
             0x01 => {
@@ -160,13 +209,12 @@ impl ClientHandler {
                 let mut bytes = [0u8; 8];
                 data.read_exact(&mut bytes)?;
                 let timestamp = i64::from_be_bytes(bytes);
-                println!("Timestamp from ping request: {timestamp}");
+                printkv("Ping timestamp", timestamp);
 
                 let mut response = Vec::new();
                 response.write_all(&[0x01])?;
                 response.write_all(&bytes)?;
-                write_response(&mut self.stream, &response)?;
-                println!("Ponged");
+                self.writer.write_response(&response)?;
             }
             x => bail!("Unknown packet ID: {x}"),
         };
@@ -174,27 +222,26 @@ impl ClientHandler {
         Ok(())
     }
 
-    fn process_login(&mut self, mut data: BufReader<&[u8]>) -> anyhow::Result<()> {
-        let packet_id = read_var_int(&mut data, 5)?;
-        println!("Packet ID: {packet_id}");
-
+    fn process_login(&mut self) -> anyhow::Result<()> {
+        let mut data = self.packet_buf.as_slice();
+        let packet_id = data.read_var_int()?;
+        printkv("Packet ID", packet_id);
         match packet_id {
             0x00 => {
-                let name = read_var_string(&mut data)?;
+                let name = data.read_var_string()?;
                 let mut bytes = [0u8; 16];
                 data.read_exact(&mut bytes)?;
                 let uuid = u128::from_be_bytes(bytes);
 
-                println!("Name: {name}");
-                println!("UUID: {uuid}");
+                printkv("Name", name);
+                printkv("UUID", uuid);
 
                 let mut response = Vec::new();
                 response.write_all(&[0x00])?;
-                write_var_string(
-                    &mut response,
+                response.write_var_string(
                     r#""Server is starting up!\n Try logging back in after a minute.""#,
                 )?;
-                write_response(&mut self.stream, &response)?;
+                self.writer.write_response(&response)?;
                 self.exit = true;
             }
             x => bail!("Unknown packet ID: {x}"),
@@ -203,8 +250,12 @@ impl ClientHandler {
         Ok(())
     }
 
-    fn process_transfer(&mut self, mut data: BufReader<&[u8]>) -> anyhow::Result<()> {
+    fn process_transfer(&mut self) -> anyhow::Result<()> {
         // Surely this works? I don't have a way to test this :(
-        self.process_login(data)
+        self.process_login()
     }
+}
+
+fn printkv(key: &str, value: impl std::fmt::Display) {
+    println!("{:<20}{:>15}", format!("{key}:"), value);
 }
